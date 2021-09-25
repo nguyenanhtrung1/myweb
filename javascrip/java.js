25b78a245deed796b56fd8cc7c7f88076d88ac18
trung nguyễn anh <trungnguyen.14141@gmail.com>
2021-09-25 17:45:22 +0700

    upload

--- javascrip/java.js
+++ javascrip/java.js
@@ -155,45 +155,6 @@ function setSuccesslogin(input){
     form2Control.className = ('secson-login success');
 }
 
-//time
-
-function time() {
-    let today = new Date();
-    let weekday=new Array(7);
-    weekday[0]='Sunday';
-    weekday[1]='Monday';
-    weekday[2]='Tuesday';
-    weekday[3]='Wednesday';
-    weekday[4]='Thursday';
-    weekday[5]='Friday';
-    weekday[6]='Saturday';
-    let day = weekday[today.getDay()];
-    let dd = today.getDate();
-    let mm = today.getMonth()+1; 
-    let yyyy = today.getFullYear();
-    let h=today.getHours();
-    let m=today.getMinutes();
-    let s=today.getSeconds();
-    m=checkTime(m);
-    s=checkTime(s);
-    nowTime = h+':'+m+':'+s;
-    if(dd<10){dd='0'+dd}
-    if(mm<10){mm='0'+mm}
-    today = day+', '+ yyyy+'/'+mm+'/'+dd;
-    tmp='<span class="date">'+today+' | '+nowTime+'</span>';
-
-    selectAll('.clock').innerHTML=tmp;
-
-    clocktime=setTimeout('time()','1000','JavaScript');
-    function checkTime(i)
-    {
-    if(i<10){
-    i='0' + i;  
-    }
-    return i;
-    }
-}
-
 //slider-menu
 
 const slider = selectAll('.slider');
